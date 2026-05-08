adb1af7826bf685a272725027f88a4e5bde93fad
Aparna Kotta <appukotta99@gmail.com>
2026-05-08 20:25:07 +0800

    first commit

---
+++ quickserve/models.py
@@ -0,0 +1,30 @@
+from django.db import models # type: ignore
+
+# Create your models here.
+class Customer(models.Model):
+    username = models.CharField(max_length = 20)
+    password = models.CharField(max_length = 20)
+    email = models.CharField(max_length = 20)
+    mobile = models.CharField(max_length = 10)
+    address = models.CharField(max_length = 50)
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length = 20)
+    picture = models.URLField(max_length = 200,        default='https://www.image2url.com/r2/default/images/1777972917474-ed089f2a-e9bc-4515-a35e-c57f62b57c84.jpeg')
+    cuisine = models.CharField(max_length = 200)
+    rating = models.FloatField()
+
+class Item(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete = models.CASCADE, related_name = "items")
+    name = models.CharField(max_length = 20)
+    description = models.CharField(max_length = 200)
+    price = models.FloatField()
+    vegeterian = models.BooleanField(default=False)
+    picture = models.URLField(max_length = 400, default='https://www.indiafilings.com/learn/wp-content/uploads/2024/08/How-to-Start-Food-Business.jpg')
+
+class Cart(models.Model):
+    customer = models.ForeignKey(Customer, on_delete = models.CASCADE, related_name = "cart")
+    items = models.ManyToManyField("Item", related_name = "carts")
+
+    def total_price(self):
+        return sum(item.price for item in self.items.all())
